913d7241db5462787657e5cb76e5bdca65af3acc
Prafulla Joshi <joshiprafulla21@gmail.com>
2018-02-07 21:51:24 +0530

    Add files via upload

---
+++ 04-Window_TextOut/Window_TextOut.cpp
@@ -0,0 +1,81 @@
+#include"stdafx.h"
+#include<windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+	//local variables
+
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	//char AppName[] = "Windows";
+	TCHAR AppName[] = TEXT("Windows");
+
+	//Code
+
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpszClassName = AppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	//Registration
+
+	RegisterClassEx(&wndclass);
+
+
+	//Creation of Window
+
+	hwnd = CreateWindow(AppName, TEXT("Prafulla's First Window"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+
+	//Displaying of Window
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	// Message of loop
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+
+	}
+	return (msg.wParam);
+
+	//End of WinMain
+}
+
+//Window Procedure (Window Function)
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	//RECT rc;
+	TCHAR str[] = TEXT("I love my India..!!!");
+	switch (iMsg)
+	{
+	case WM_CREATE:
+		hdc = GetDC(hwnd);
+		//GetClientRect(hwnd, &rc);
+		TextOut(hdc, 10, 10, str, wcslen(str));
+		SetTextColor(hdc, RGB(0, 255, 0));
+		SetBkColor(hdc, RGB(255, 0, 0));
+		ReleaseDC(hwnd, hdc);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return (DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+//End of Complete Program
